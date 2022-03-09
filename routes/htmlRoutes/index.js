bf8474572e68052975df410e6b6f7d3d63070fa3
lillyleiran <lilly.leiran@gmail.com>
2022-03-08 21:16:18 -0700

    integrate zookeeper page functionality

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,25 +1,20 @@
-const path = require('path');
-const router = require('express').Router();
+const path = require("path");
+const router = require("express").Router();
 
-
-// display the index HTML page
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
-// display the animals HTML page
-router.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
+router.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/animals.html"));
 });
 
-// display the zookeepers HTML page
-router.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+router.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/zookeepers.html"));
 });
 
-// display if HTML page route is unavailable (in case of errors)
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 module.exports = router;
